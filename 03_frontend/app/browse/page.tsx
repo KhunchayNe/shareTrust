ecfec532101b3ba99ea764833f9e9d2608740c16
siwapan-imac <work.siwapun.kh@gmail.com>
2025-11-11 23:22:33 +0700

    feat: Add AdminPage and BrowsePage components with basic layout

---
+++ 03_frontend/app/browse/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { AppLayout } from '@/components/layout/AppLayout'
+
+export default function BrowsePage() {
+  return (
+    <AppLayout
+          headerTitle="Profile"
+          showBottomNav={true}
+        >
+    <div>Browser Page</div>
+  </AppLayout>
+  )
+}
